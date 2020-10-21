b27b677d45b272694018b4f1a540d8fd9175a7a0
Mikhail Ablakatov <ablakatov@phystech.edu>
2020-10-21 13:02:03 +0300

    test.c: implemented init functions; added dummy open/release methods

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include <linux/cdev.h>
 #include <linux/fs.h>
+#include <linux/init.h>
+#include <linux/kdev_t.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/printk.h>
+
 MODULE_DESCRIPTION("My kernel module");
 MODULE_AUTHOR("Me");
 MODULE_LICENSE("GPL");
@@ -16,21 +21,68 @@ struct dummy_device_data {
 
 static struct dummy_device_data dummy_device_data[DUMMY_NUM_MINORS];
 
+static int dummy_open(struct inode *inode, struct file *file)
+{
+	printk("%s: opened.\n", DUMMY_MODULE_NAME);
+
+	return 0;
+}
+
+static int dummy_release (struct inode *inode, struct file *file)
+{
+	printk("%s: closed.\n", DUMMY_MODULE_NAME);
+
+	return 0;
+}
+
 static struct file_operations dummy_fops = {
 	.owner = THIS_MODULE,
+	.open = dummy_open,
+	.release = dummy_release
 };
 
 static int dummy_chrdev_init(void)
 {
-	/* alloc_chrdev_region() */
+	int error;
+	dev_t first_dev;
+	unsigned int major, first_minor;
+	int i;
+
+	error = alloc_chrdev_region(&first_dev, DUMMY_MINOR, DUMMY_NUM_MINORS,
+				    DUMMY_MODULE_NAME);
+	if (error) {
+		pr_err("%s: alloc_chrdev_region failed: %d\n",
+		       __FUNCTION__, error);
+		return error;
+	}
+
+	major = MAJOR(first_dev);
+	first_minor = MINOR(first_dev);
+	BUG_ON(first_minor + DUMMY_NUM_MINORS > MINORMASK);
+
+	for (i = 0; i < DUMMY_NUM_MINORS; i++) {
+		dummy_device_data[i].dev = MKDEV(major, first_minor + i);
+	}
+
 	return 0;
 }
 
 static int dummy_cdev_init(struct dummy_device_data *device_data)
 {
-	/* cdev_init() */
-	/* cdev_add() */
-	return 0;
+	int error;
+	struct cdev *cdev = &device_data->cdev;
+
+	cdev_init(cdev, &dummy_fops);
+	error = cdev_add(cdev, device_data->dev, 1);
+	if (error) {
+		pr_err("%s: cdev_add failed: %d\n",
+		       __FUNCTION__, error);
+	}
+
+	printk("%s: created character device %x:%x (ma:mi).\n", DUMMY_MODULE_NAME,
+	       MAJOR(device_data->dev), MINOR(device_data->dev));
+
+	return error;
 }
 
 static int dummy_init(void)
